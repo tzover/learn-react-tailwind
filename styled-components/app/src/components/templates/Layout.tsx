9a4ef56042d26c9aa258fadd86faefdeccebcf0c
yt <asnedge.yt@gmail.com>
2022-02-18 16:12:59 +0900

    update styled-components version

---
+++ styled-components/app/src/components/templates/Layout.tsx
@@ -0,0 +1,22 @@
+import Header from '../modules/Header'
+import { ReactNode } from 'react'
+import Footer from '../modules/Footer'
+
+interface Props {
+  pageTitle: string
+  appName: string
+  children: ReactNode
+}
+
+const Layout = (props: Props) => {
+  const { pageTitle, appName, children } = props
+  return (
+    <>
+      <Header pageTitle={pageTitle} appName={appName} />
+      <div className='px-10 py-8'>{children}</div>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
---
+++ tailwind/app/docs/README.md
@@ -0,0 +1,6 @@
+# install styled-components
+
+```
+yarn add styled-components
+yarn add -D @types/styled-components
+```
